db783556db9204b6192d4ff9c67d38dc17791847
Matt Peretick <mattjp@users.noreply.github.com>
2016-05-19 08:51:23 -0400

    Add files via upload

--- modulator.js
+++ modulator.js
@@ -1,3 +1,7 @@
 function compute() {
-	
+	var result;
+	var num1 = document.getElementById("input1").value;
+	var num2 = document.getElementById("input2").value;
+	result = num1 % num2;
+	document.getElementById("result").innerHTML = result;
 }
